f6c32076eee4f651772d6f91b78eafd29dc3ef3f
Daniel David Herrera Acevedo <herreraacevedodl@gmail.com>
2023-12-17 00:38:20 -0500

    Update styles and dependencies

--- app/Components/UI/ProjectCard.jsx
+++ app/Components/UI/ProjectCard.jsx
@@ -2,18 +2,19 @@ import Link from 'next/link'
 
 const ProjectCard = (props) => {
 
-  const cardTitlesArray = ['SunSpotCalc (Web & UI)', 'CentiApp (Web & UI)', `Dani's place (Web & UI)`, 'Enawa (UI)'];
+  const cardTitlesArray = ['SunSpotCalc (Web & UI)', 'CentiApp (Web & UI)', `Dani's place (Web & UI)`, 'Enawa (UI)', 'Iter (Web & UI)'];
   const cardDescriptionsArray = [
     'SunSpotCalc es una app web que permite calcular la rotacion diferencial del sol usando como referencia las manchas que aparecen en las fotos del mismo.',
     'Centi es la app ideal para los aficionados a la astronomía. Obtén información en tiempo real sobre las condiciones del cielo y los astros visibles. ¡Ten el cielo nocturno en la palma de tu mano!',
     'Es justo lo que estas viendo... 😎',
     'Enawa es una idea de negocio que se basa en una app para reportar problemas ambientales en las diferentes ciudades.',
+    'Iter es una app que permite a los usuarios saber informacion sobre los planetas del sistema solar y el tiempo que se tardaria viajando a ellos.'
   ];
   const cardLinksArray = [
     'https://theskylabteam.github.io/sunspotcalc/#/',
     'https://centi-app.vercel.app',
     'https://danis-place.vercel.app',
-    '/',
+    'https://iter-five.vercel.app/',
   ];
 
   let cardTitle = '';
@@ -44,6 +45,11 @@ const ProjectCard = (props) => {
     cardDescription = cardDescriptionsArray[3];
     detailLink = '/enawa';
     cardLink = cardLinksArray[3];
+  } if (props.project == 'iter') {
+    cardTitle = cardTitlesArray[4];
+    cardDescription = cardDescriptionsArray[4];
+    detailLink = '/iter';
+    cardLink = cardLinksArray[3];
   }
 
 
@@ -55,7 +61,6 @@ const ProjectCard = (props) => {
         </div>
         <div className="mt-4 md:mt-8 lg:mt-4 xl:mt-8">
             <ul className="flex flex-row justify-end gap-4">
-                <li className="flex text-lightPurple text-sm xl:text-base lg:text-xs md:text-xs"><Link href={detailLink}>Más detalles</Link></li>
                 <li className="flex text-lightPurple text-sm xl:text-base lg:text-xs md:text-xs"><a href={cardLink} target='_blank'>Visitar pagina</a></li>
             </ul>
         </div>
--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,9 @@
-import Link from 'next/link'
 import MainNavBar from './Components/UI/MainNavbar'
 import ProjectCard from './Components/UI/ProjectCard'
 
 export default function Home() {
   return (
-    <main className='bg-purple w-full h-full'>
+    <main className='bg-purple w-full h-full flex flex-col items-center'>
       <MainNavBar/>
       {/* Presentation container that have the names on it and the abilities */}
       <div id="presentationContainer" className="w-full h-[20rem] flex md:h-[30rem] flex-col px-4 py-8 justify-center md:justify-center md:px-20 md:py-4 lg:px-40 lg:py-10">
@@ -17,12 +16,9 @@ export default function Home() {
         </div>
         <ul id="abilitiesList" className="flex flex-row text-bright">
           <li className='text-[0.72rem] pr-4 md:text-lg'>Desarrollador Web</li>
-          <li className='pr-4 text-[0.72rem] md:text-lg'>Diseñador UI</li>
+          <li className='pr-4 text-[0.72rem] md:text-lg'>Diseñador UI/UX</li>
           <li className='pr-4 text-[0.72rem] md:text-lg'>Investigador</li>
         </ul>
-        <div id="downloadCvButton" className="border-solid border-dark border-2 text-bright w-48 md:w-64 h-10 md:h-14 flex my-4 rounded-full justify-center  items-center">
-          <h2 className='font-semibold text-sm text-center aling-middle'>Descarga mi CV</h2>
-        </div>
       </div>
       
       {/* Projects Section */}
@@ -30,7 +26,7 @@ export default function Home() {
         <h2 className='project-title'>Mis proyectos</h2>
         <div id="projectsContainer" className='grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 w-full gap-8 pt-8 lg:pt-16 md:pt-8 lg:gap-4 md:gap-4'>
           <ProjectCard project='sunspotcalc'/>
-          <ProjectCard project='centi'/>
+          <ProjectCard project='iter'/>
           <ProjectCard project='danisplace'/>
           <ProjectCard project='enawa'/>
         </div>
@@ -45,6 +41,7 @@ export default function Home() {
           <div className="ability-button">Js</div>
           <div className="ability-button">ReactJs</div>
           <div className="ability-button">Next Js</div>
+          <div className="ability-button">D3 JS</div>
           <div className="ability-button">Figma</div>
         </div>
       </div>
